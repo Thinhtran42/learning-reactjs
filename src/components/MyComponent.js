586e65c93be2052288d36628ad9d5da7a5e76836
Thinh Tran <thinhtran@Thinhs-MacBook-Pro-2.local>
2022-12-30 22:30:59 +0700

    Hoc reactjs setState

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,8 +1,33 @@
 import React, { Component } from "react";
 
 export class MyComponent extends Component {
+  state = {
+    name: "Thinh Tran",
+    learning: "Reactjs",
+    age: 20,
+  };
+  onClickHandle = () => {
+    console.log("Click me ! ");
+
+    this.setState({
+      name: "Thinh Dep trai",
+      learning: "Java Spring Boot",
+    });
+  };
+  onMouseOverHandle(event) {
+    // console.log(event.target);
+  }
   render() {
-    return <div>MyComponent</div>;
+    return (
+      <>
+        <div>MyComponent</div>
+        <div>
+          My name is {this.state.name} and I'm learning {this.state.learning}
+        </div>
+        <button onClick={this.onClickHandle}>click me </button>
+        <button onMouseOver={this.onMouseOverHandle}>Hover me</button>
+      </>
+    );
   }
 }
 
