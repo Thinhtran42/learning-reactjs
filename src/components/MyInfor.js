939836f91990db7b9cf88ba67930f9502080bfb0
Thinh Tran <thinhtran@Thinhs-MacBook-Pro-2.local>
2023-01-02 17:38:25 +0700

    hoc react cau dieu kien &&

--- src/components/MyInfor.js
+++ src/components/MyInfor.js
@@ -1,22 +1,39 @@
 import React, { Component } from "react";
 
 class MyInfor extends Component {
+  state = {
+    isShowListUser: true,
+  };
+  handleOnClick = () => {
+    this.setState({ isShowListUser: !this.state.isShowListUser });
+  };
   render() {
     const { listUsers } = this.props;
     console.log(listUsers);
     return (
       <>
-        {listUsers.map((user) => {
-          return (
-            <div key={user.id}>
-              <h1>My name's {user.name} </h1>
-              <h1>My age's {user.age}</h1>
-              <hr />
-            </div>
-          );
-        })}
-        {/* <h1>My name's is </h1>
-        <h1>My age's is </h1> */}
+        <div
+          onClick={() => {
+            this.handleOnClick();
+          }}
+        >
+          {this.state.isShowListUser === true
+            ? "Hide list user"
+            : "Show list user"}
+        </div>
+        {this.state.isShowListUser && (
+          <div>
+            {listUsers.map((user) => {
+              return (
+                <div key={user.id} className={+user.age > 18 ? "red" : "green"}>
+                  <h1>My name's {user.name} </h1>
+                  <h1>My age's {user.age}</h1>
+                  <hr />
+                </div>
+              );
+            })}
+          </div>
+        )}
       </>
     );
   }
